7b47d689acf5bdce92b9c16e869d9908c953566b
Spencer Gordon <sgord512@gmail.com>
2012-07-09 19:47:04 +0200

    added unfoldrM

--- Util/Monad.hs
+++ Util/Monad.hs
@@ -4,4 +4,13 @@ module Util.Monad where
 (~>) :: (Monad m) => a -> [(a -> m a)] -> m a
 (~>) a [] = return a
 a ~> (f:fs) = do f a >>= (~> fs)
-                 
+
+unfoldrM :: (Monad m) => (b -> m (Maybe (a, b))) -> b -> m [a]
+unfoldrM f state = do
+  result <- f state
+  case result of                
+    Nothing -> return []
+    Just (val, state') -> do 
+      vals <- unfoldrM f state'
+      return $ (val:vals)
+
